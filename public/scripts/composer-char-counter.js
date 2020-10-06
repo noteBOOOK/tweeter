2e4832a8cc1f9b6d6864e65f6ec9115c6985ce55
Jason Long <jason.long77@hotmail.com>
2020-10-06 19:23:25 +0000

    Created and implemented event for character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,15 @@
-console.log("I'm in!");
+$(document).ready(function() {
+  
+  
+  
+  
+  $("#tweet-text").on("keyup", function() {
+    const remainingChars = 140 - $(this).val().length;
+    $(this).siblings("div").children("output").text(remainingChars);
+    if (remainingChars <= 0) {
+      $(this).siblings("div").children("output").attr("charLimit", true);
+    } else {
+      $(this).siblings("div").children("output").attr("charLimit", false);
+    }
+  })
+});

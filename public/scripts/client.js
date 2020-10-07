de76a4f8abeab573bc7e2708d1b4733bd0290f5f
Jason Long <jason.long77@hotmail.com>
2020-10-07 20:27:07 +0000

    Created loadTweets function with Ajax GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,6 +10,16 @@
 const data = [
   {
     "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
       "name": "Newton",
       "avatars": "https://i.imgur.com/73hZDYK.png"
       ,
@@ -20,54 +30,51 @@ const data = [
     },
     "created_at": 1461116232227
   },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
 ];
 
 
-const createTweetElement = function (tweetObj) {
-  let $tweet = `<article class="tweet">
-            <header class="tweet-header">
-              <div class="tweet-user">
-                <img src="${tweetObj.user.avatars}">
-                <span class="tweeter-name">${tweetObj.user.name}</span>
-              </div>
-              <span class="tweeter-handle">${tweetObj.user.handle}</span>
-            </header>
-            <p>${tweetObj.content.text}</p>
-            <footer>
-              <span>${tweetObj.created_at}</span>
-              <div class="symbols">
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
-            </footer>
-            </article>`;
-  
-  return $tweet;
-};
 
-const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-    let $tweet = createTweetElement(tweet);
-    $('.tweet-log').append($tweet);
-  }
-};
 
 
 
 $(document).ready(function() {
-  renderTweets(data);
   
+  const createTweetElement = function (tweetObj) {
+    let $tweet = `<article class="tweet">
+              <header class="tweet-header">
+                <div class="tweet-user">
+                  <img src="${tweetObj.user.avatars}">
+                  <span class="tweeter-name">${tweetObj.user.name}</span>
+                </div>
+                <span class="tweeter-handle">${tweetObj.user.handle}</span>
+              </header>
+              <p>${tweetObj.content.text}</p>
+              <footer>
+                <span>${tweetObj.created_at}</span>
+                <div class="symbols">
+                  <i class="fas fa-flag"></i>
+                  <i class="fas fa-retweet"></i>
+                  <i class="fas fa-heart"></i>
+                </div>
+              </footer>
+              </article>`;
+    
+    return $tweet;
+  };
+  
+  
+  
+  
+  
+  
+  
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $('.tweet-log').append($tweet);
+    }
+  };
+
   $('form').submit(function(event) {
     event.preventDefault();
     const tweet = $(this).serialize();
@@ -81,7 +88,16 @@ $(document).ready(function() {
     })
   })
   
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    }).then(res => {
+      renderTweets(res);
+    })
+  }
 
+  loadTweets();
 
 
 });

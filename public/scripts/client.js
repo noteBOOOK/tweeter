2c065e736eb7c2b765fd29a458509d3fe73d6397
Jason Long <jason.long77@hotmail.com>
2020-10-08 23:16:34 +0000

    Created error message function, cleaned up code and added comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
 const data = [
   {
     "user": {
@@ -32,97 +30,107 @@ const data = [
 ];
 
 
+// Escape function to prevent Cross Site Scripting
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 
+// Create Tweet element to render
+const createTweetElement = function(tweetObj) {
+  let date = new Date(tweetObj.created_at);
+  
+  let $tweet = `<article class="tweet">
+  <header class="tweet-header">
+  <div class="tweet-user">
+  <img src="${tweetObj.user.avatars}">
+  <span class="tweeter-name">${tweetObj.user.name}</span>
+  </div>
+  <span class="tweeter-handle">${tweetObj.user.handle}</span>
+  </header>
+  <p class="tweet-content">${escape(tweetObj.content.text)}</p>
+  <footer class="tweet-footer">
+  <span>${moment(date).fromNow()}</span>
+  <div class="symbols">
+  <i class="fas fa-flag"></i>
+  <i class="fas fa-retweet"></i>
+  <i class="fas fa-heart"></i>
+  </div>
+  </footer>
+  </article>`;
+  
+  return $tweet;
+};
 
+// Renders loaded tweets from GET request
+const renderTweets = function(tweets) {
+  $(".tweet-log").html("");
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(".tweet-log").prepend($tweet);
+  }
+};
+
+// Makes GET request for database of tweets
+const loadTweets = function() {
+  $.ajax({
+    url: "/tweets",
+    method: "GET"
+  }).then(res => {
+    renderTweets(res);
+  });
+};
+
+// Checks for errors before making AJAX POST request
+const createErrorMessage = function(tweet) {
+  if (tweet.length <= 5) {
+    $("#error-message").text("You cannot post an empty Tweet!");
+    $(".error").slideDown(500);
+    return false;
+  } else if (tweet.length > 145) {
+    $("#error-message").text("Your Tweet is over the character limit! (140)");
+    $(".error").slideDown(500);
+    return false;
+  }
+  return true;
+};
+
+////////////////////////////////////////////////////////////////////
 
 $(document).ready(function() {
   
-  
-  const escape = function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  }
-  
-  const createTweetElement = function (tweetObj) {
-    let date = new Date(tweetObj.created_at);
-    
-    let $tweet = `<article class="tweet">
-    <header class="tweet-header">
-    <div class="tweet-user">
-    <img src="${tweetObj.user.avatars}">
-    <span class="tweeter-name">${tweetObj.user.name}</span>
-    </div>
-    <span class="tweeter-handle">${tweetObj.user.handle}</span>
-    </header>
-    <p class="tweet-content">${escape(tweetObj.content.text)}</p>
-    <footer class="tweet-footer">
-    <span>${moment(date).fromNow()}</span>
-    <div class="symbols">
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-    </div>
-    </footer>
-    </article>`;
-    
-    return $tweet;
-  };
-  
-  const renderTweets = function(tweets) {
-    $('.tweet-log').html('');
-    for (let tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $('.tweet-log').prepend($tweet);
-    }
-  };
-  
-  const loadTweets = function () {
-    $.ajax({
-      url: "/tweets",
-      method: "GET"
-    }).then(res => {
-      renderTweets(res);
-    })
-  }
-  
-  $('form').submit(function(event) {
+  // AJAX POST request to submit new tweets
+  $("form").submit(function(event) {
     event.preventDefault();
-    $('.error').slideUp(100);
+    $(".error").slideUp(50);
     const tweet = $(this).serialize();
-    if (tweet.length <= 5) {
-      $('#error-message').text("You cannot post an empty Tweet!");
-      $('.error').slideDown(500);
-    } else if (tweet.length > 145) {
-      $('#error-message').text("Your Tweet is over the character limit! (140)")
-      $('.error').slideDown(500);
-    } else {
+    if (createErrorMessage(tweet)) {
       $.ajax({
         url: "/tweets",
         method: "POST",
         data: tweet
       }).then(res => {
         loadTweets();
-        $('#tweet-text').val("").trigger("keyup");
-      })
+        $("#tweet-text").val("").trigger("keyup");
+      });
     }
-  })
+  });
   
-  $('.new-tweet-button').on("click", function () {
-    if ($('.tweet-form').is(":hidden")) {
-      $('.tweet-form').slideDown(500, function () {
-        $('#tweet-text').focus();
+  // Resets textarea and character counter when submitting new tweets
+  $(".new-tweet-button").on("click", function() {
+    if ($(".tweet-form").is(":hidden")) {
+      $(".tweet-form").slideDown(500, function() {
+        $("#tweet-text").focus();
       });
     } else {
-      $('.tweet-form').slideUp(500);
+      $(".tweet-form").slideUp(500);
     }
-  })
-  
+  });
   
   loadTweets();
-  $('.error').hide();
-  $('.tweet-form').hide();
-  
+  $(".error").hide();
+  $(".tweet-form").hide();
   
 });
